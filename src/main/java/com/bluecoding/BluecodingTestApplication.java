f0499067f29dbe28a67879641000d27120e50012
Ricardo Araujo <ricardofranc.araujo@gmail.com>
2023-06-16 09:57:44 -0300

    Initial Commit

---
+++ src/main/java/com/bluecoding/BluecodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.bluecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluecodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluecodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluecoding/BluecodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluecodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
